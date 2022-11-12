2fff8f1de1ace87dcb9cfd68357161222e2cc5e4
mcuzbgamer <111974683+mcuzbgamer@users.noreply.github.com>
2022-11-12 11:10:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,59 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-    host: 'ir.skyblock.uz',
-    port: 25566,
-    version: 1.17,
-    username: 'botFazliddinov_4'
+ host:'ir.skyblock.uz',
+ port: 25566,
+ username: 'botFazliddinov_4'
 })
-
-let password = "jobir2009" // sizning parolingiz
-
 bot.on('messagestr', (message) => {
-    console.log(message)
+  if(message.includes("/register"))
+  {
+    bot.chat("/reg jobir2009 jobir2009")
+  }
+  
 
-    if (message.includes("/register")) {
-        bot.chat(`/register ${password} ${password}`)
-    }
-    
-    if (message.includes("/login")) {
-        bot.chat(`/login ${password}`)
-    }
-})
+}
 
-//  tegilmasin START
+
+)
+bot.on('messagestr', (message) => {
+  if(message.includes("/log"))
+  {
+    bot.chat("/login jobir2009")
+  }
+  
+
+}
+
+
+)
 async function dig() {
   if (!bot.heldItem || !bot.heldItem.name.includes('pickaxe')) {
     var pickaxe = bot.inventory.items().filter(i => i.name.includes('pickaxe'))[0];
     if (pickaxe) await bot.equip(pickaxe, 'hand')
-    if(!pickaxe) bot.quit(); // TMP : QUIT IF NO PICKAXES.
+    if(!pickaxe) bot.quit();
   }
-  var block = bot.blockAtCursor(4);
+  var block = bot.blockAtCursor(7);
   if (!block) return setTimeout(function() {
     dig();
   }, 100);
-  await bot.dig(block, 'ignore', 'raycast') // 2nd param: true to 'snap at block' or 'ignore' to just not turn head
+  await bot.dig(block, 'ignore', 'raycast')
   dig()
 }
-
-
-
-
-
-async function lookAtNearestPlayer () {
-  const playerFilter = (entity) => entity.type === 'player'
-  const playerEntity = bot.nearestEntity(playerFilter)
-  
-  if (!playerEntity) return
-  
-  const pos = playerEntity.position.offset(0, playerEntity.height, 0)
-  bot.lookAt(pos)
-}
-
-
- // END
-
-bot.on('chat', (username, message) => {
-    if (username == 'JabikJan' || username=='FamousTrain1733')  { // <== sizning parolingiz
-        if (message == 'Jabikminer_3') { // <== buyruq istasangiz tegmang
-            dig();
-        }
-    }
+bot.once("spawn", () => {
+    setTimeout(() => {
+        dig();
+   }, 20000); 
 })
+bot.on('chat', (username, message) => {
+    if (username === 'FamousTrain1733') {
+    if (message.indexOf('4') !== -1) {
+            var replacement = "4",
+                toReplace = "",
+                str = message
 
-bot.on('chat',(username,message)=>{
-  if (username == 'JabikJan'|| username == 'FamousTrain1733'){
-    if (message === 'qara'){
-    lookAtNearestPlayer()
-}
-}})
-bot.on('chat',(username,message)=>{
-  if (username == 'JabikJan'|| username == 'FamousTrain1733'){
-    if (message === 'qarama'){
-    bot.chat('xop')
-}
-}})
-
-bot.on('chat',(username,message)=>{
-  if(username == 'JabikJan'){
-     if(message == 'oldimga3'){
-	bot.chat('/tpa JabikJan')
-}}else if(username == 'FamousTrain1733'){
-    if (message == 'oldimga3'){
-    bot.chat('/tpa FamousTrain1733')
-}
-}})
-
-bot.on('chat',(message,username)=>{
-  if (username == 'JabikJan'|| username == 'FamousTrain1733'){
-    if (message === 'back'){
-    bot.chat('/back')
-}}})
+            str = str.replace(replacement, toReplace)
+            bot.chat(str)
+        }}})
+    bot.on('kicked', console.log)
+bot.on('error', console.log)
